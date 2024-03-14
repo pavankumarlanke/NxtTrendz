d3de7883671d3288ecf5748c09b0ae0117b4e500
Pavan Kumar Lanke <109908347+pavankumarlanke@users.noreply.github.com>
2024-03-14 10:00:02 +0530

    Add files via upload

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,1 +1,12 @@
+import React from 'react'
 
+const CartContext = React.createContext({
+  cartList: [],
+  removeAllCartItems: () => {},
+  addCartItem: () => {},
+  removeCartItem: () => {},
+  incrementCartItemQuantity: () => {},
+  decrementCartItemQuantity: () => {},
+})
+
+export default CartContext
